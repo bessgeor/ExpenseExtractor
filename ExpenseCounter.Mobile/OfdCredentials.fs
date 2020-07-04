be06e2a0afee0b1a76594de91610e199ac5f755a
bessgeor <bessgeor@gmail.com>
2020-07-04 17:05:16 +0300

    Add OFD credentials storage

---
+++ ExpenseCounter.Mobile/OfdCredentials.fs
@@ -0,0 +1,36 @@
+﻿module OfdCredentials
+
+open Xamarin.Essentials
+
+  [<Struct>]
+  type OfdCredentials = {
+    Phone: string
+    Password: string
+  }
+
+  let private ofdCredentials phone pwd =
+    {
+      Phone = phone
+      Password = pwd
+    }
+
+  let private phoneKey = "ofd_credentials_phone"
+  let private paswdKey = "ofd_credentials_password"
+
+  let get () =
+    async {
+      let! phone = SecureStorage.GetAsync phoneKey |> Async.AwaitTask
+      let phoneOpt = ValueOption.ofObj phone
+      if ValueOption.isNone phoneOpt
+        then return ValueNone
+        else
+          let! paswd = SecureStorage.GetAsync paswdKey |> Async.AwaitTask
+          let paswdOpt = ValueOption.ofObj paswd
+          return ValueOption.map2 ofdCredentials phoneOpt paswdOpt
+    }
+
+  let set credentials =
+    async {
+      do! SecureStorage.SetAsync(phoneKey, credentials.Phone) |> Async.AwaitTask
+      do! SecureStorage.SetAsync(paswdKey, credentials.Password) |> Async.AwaitTask
+    }
