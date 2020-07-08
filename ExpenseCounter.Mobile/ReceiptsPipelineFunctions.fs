8923528426fea1bea0e3d4b223c26b5f25846794
bessgeor <bessgeor@gmail.com>
2020-07-08 23:44:09 +0300

    Add details getting pipeline function

--- ExpenseCounter.Mobile/ReceiptsPipelineFunctions.fs
+++ ExpenseCounter.Mobile/ReceiptsPipelineFunctions.fs
@@ -14,6 +14,11 @@
       FiscalSignature = extractQueryStringComponent "fp" stringReviver qs
     }
 
+  let private handleAggrException (exn: Exception) (e: Exception) (v: 'value) (f: 'value*Exception -> 'result) =
+    match exn with
+    | :? AggregateException as aggr -> f (v, AggregateException([| yield! aggr.InnerExceptions; yield e |]))
+    | old -> f (v, AggregateException(old, e))
+
   let tryParse receipt =
     match receipt with
     | RawScanned qs ->
@@ -26,7 +31,68 @@
         let parsed = parse qs
         Parsed parsed
       with | :? Exception as e ->
-        match exn with
-        | :? AggregateException as aggr -> ParseFailed (qs, AggregateException([| yield! aggr.InnerExceptions; yield e |]))
-        | old -> ParseFailed (qs, AggregateException(old, e))
+        handleAggrException exn e qs ParseFailed
     | any -> any
+
+  let private handleFNSResponse taskName receipt onSuccess (responseTask: Threading.Tasks.Task<#Results.Result>) =
+    async {
+      try
+        let! response = Async.AwaitTask responseTask
+        if not response.IsSuccess then
+          return DetailsGettingFailed (receipt, Exception(sprintf "%s: %A (%s)" taskName response.StatusCode response.Message))
+        else
+          return! onSuccess response
+      with | :? Exception as e -> return DetailsGettingFailed (receipt, e)
+    }
+
+  let private parseDetailsFromResponse (doc: Results.Receipt) receipt =
+    {
+      Identifiers = receipt
+      SellerTIN = doc.RetailInn
+      RetailAddress = Option.ofObj doc.RetailPlaceAddress
+      StoreName = Option.ofObj doc.StoreName
+      IssuedAt = doc.ReceiptDateTime
+      Positions = 
+        doc.Items
+        |> Seq.map (fun i -> {
+          Name = i.Name
+          Quantity = i.Quantity
+          Price = (decimal i.Price) / 100M
+          Sum = (decimal i.Sum) / 100M
+        })
+        |> Seq.toArray
+    }
+
+  let private getDetails (receipt: ParsedReceipt) =
+    async {
+      let! credentialsOption = OfdCredentials.get()
+      if ValueOption.isNone credentialsOption then
+        return DetailsGettingFailed (receipt, Exception("No credentials stored"))
+      else
+          let credentials = ValueOption.get credentialsOption
+          return!
+            FNS.LoginAsync (credentials.Phone, credentials.Password)
+            |> handleFNSResponse "login" receipt (fun _ ->
+              FNS.CheckAsync (receipt.FiscalNumber, receipt.FiscalDocumentNumber, receipt.FiscalSignature, receipt.Time, receipt.Sum)
+              |> handleFNSResponse "check receipt" receipt (fun response ->
+                if not response.ReceiptExists then
+                  async.Return (DetailsGettingFailed (receipt, Exception("Federal Tax Service claims that this receipt does not exist")))
+                else
+                  FNS.ReceiveAsync (receipt.FiscalNumber, receipt.FiscalDocumentNumber, receipt.FiscalSignature, credentials.Phone, credentials.Password)
+                  |> handleFNSResponse "receive receipt details" receipt (fun resp -> parseDetailsFromResponse resp.Document.Receipt receipt |> Detailed |> async.Return)
+              )
+            )
+    }
+
+  let tryDetail receipt =
+    async {
+      match receipt with
+      | Parsed r ->
+        return! getDetails r
+      | DetailsGettingFailed (r, exn) ->
+        let! detailed = getDetails r
+        match detailed with
+        | DetailsGettingFailed (r, e) -> return handleAggrException exn e r DetailsGettingFailed
+        | any -> return any
+      | any -> return any
+    }
